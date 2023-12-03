b78e31d38a3cf8f847149b10a456da5a54c11bf0
Cade Wilson <cade25wilson@gmail.com>
2023-12-02 23:18:56 -0700

    database creation

--- download/tasks.py
+++ download/tasks.py
@@ -2,6 +2,7 @@ import os
 import re
 from celery.utils.log import get_task_logger
 from celery import shared_task
+from .models import Video
 from pytube import YouTube
 
 logger = get_task_logger(__name__)
@@ -10,8 +11,14 @@ logger = get_task_logger(__name__)
 def download_video(url):
     yt = YouTube(url)
     video = yt.streams.get_highest_resolution()
-    filename = video.default_filename
+    filename = yt.title
     download_path = os.path.join(os.getcwd(), 'download/download', filename)
     video.download(output_path=os.path.dirname(download_path))
+    # make a title variable that removes all spaces and replaces them with underscores
+    # this will be used to name the file
+    filename = filename + '.mp4'
+    # videotitle = re.sub('\s', '_', filename1)
+    video = Video(filename=filename, title=filename)
+    video.save()
 
     return filename
