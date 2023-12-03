7e5d29fb049faecc5ddcec04b432c5c6c71ce6a5
Cade Wilson <cade25wilson@gmail.com>
2023-12-02 19:43:17 -0700

    celery

--- download/views.py
+++ download/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import FileResponse, HttpResponse
 from wsgiref.util import FileWrapper
+from celery import shared_task
 from django.views.decorators.csrf import csrf_exempt
 from pytube import YouTube
 import os
@@ -8,20 +9,44 @@ import os
 def index(request):
     return render(request, "index.html")
 
+@csrf_exempt 
+
+# def download(request):
+#     if request.method == "POST":
+#         url = request.POST.get("url")
+#         yt = YouTube(url)
+#         video = yt.streams.get_highest_resolution()
+#         filename = video.default_filename
+#         video.download()
+
+#         # Wrap the file in FileWrapper
+#         wrapper = FileWrapper(open(filename, 'rb'))
+#         response = HttpResponse(wrapper, content_type='video/mp4')
+#         response['Content-Length'] = os.path.getsize(filename)
+#         response['Content-Disposition'] = f'attachment; filename={os.path.basename(filename)}'
+#         return response
+
+#     return render(request, "index.html")
+
 @csrf_exempt
 def download(request):
     if request.method == "POST":
         url = request.POST.get("url")
-        yt = YouTube(url)
-        video = yt.streams.get_highest_resolution()
-        filename = video.default_filename
-        video.download()
-
-        # Wrap the file in FileWrapper
-        wrapper = FileWrapper(open(filename, 'rb'))
-        response = HttpResponse(wrapper, content_type='video/mp4')
-        response['Content-Length'] = os.path.getsize(filename)
-        response['Content-Disposition'] = f'attachment; filename={os.path.basename(filename)}'
-        return response
+        download_video.delay(url)
+        return HttpResponse('Video is being downloaded')
 
     return render(request, "index.html")
+
+@shared_task
+def download_video(url):
+    yt = YouTube(url)
+    video = yt.streams.get_highest_resolution()
+    filename = video.default_filename
+    video.download()
+
+    # Wrap the file in FileWrapper
+    wrapper = FileWrapper(open(filename, 'rb'))
+    response = HttpResponse(wrapper, content_type='video/mp4')
+    response['Content-Length'] = os.path.getsize(filename)
+    response['Content-Disposition'] = f'attachment; filename={os.path.basename(filename)}'
+    return response
--- mysite/settings.py
+++ mysite/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+CELERY_BROKER_URL = 'redis://default:kmCaeajDaJnIMFkJ6Ha3DKEO4gDHbaG6@viaduct.proxy.rlwy.net:23088'
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
